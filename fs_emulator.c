81b8d1a1d525010700c017705b47bb07e9c49c3a
jhuanny <92960531+jhuanny@users.noreply.github.com>
2024-03-15 01:25:18 -0700

    Add files via upload

--- fs_emulator.c
+++ fs_emulator.c
@@ -3,7 +3,8 @@
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
-#include <unistd.h> 
+#include <stdint.h>
+#include <unistd.h>
 
 #define MAX_INODE 1024
 #define MAX_NAME 32
@@ -20,10 +21,14 @@ typedef struct{
 } dirContent;
 
 int inoZero(const inodeMeta* entries, int numEntries);
+int entryHuh(const char* dirName, uint32_t dirIno);
+int findNextIno(const inodeMeta* entries, int numEntries, uint32_t* nextIno);
+int updateDir(uint32_t curDirInode, uint32_t newIno, const char* dirName);
 void cmdExit(const inodeMeta* entries, int numEntries);
 void cmdLs(uint32_t curDirIno);
 void cmdCd(const char* dirName, uint32_t* curDirInode, const inodeMeta* entries, int numEntries);
-
+void cmdTouch(const char* dirName, uint32_t* curDirInode, inodeMeta* entries, int *numEntries);
+void cmdMkdir(const char* dirName, uint32_t* curDirInode, inodeMeta* entries, int* numEntries);
 int main(int argc, char *argv[]) {
     //root inode starts at 0
     uint32_t curDirInode = 0;
@@ -58,25 +63,30 @@ int main(int argc, char *argv[]) {
     printf("inodes list file was opened\n");
     int numEntries = 0;
     inodeMeta entries[MAX_INODE];
-    //hold inidividual inodemeta from fread
-    inodeMeta buffer;
-    while (fread(&buffer, sizeof(inodeMeta), 1, inoFile) == 1){
-        if (buffer.type != 'd' && buffer.type != 'f'){
-            printf("inode file type is invalid: %u %c\n", buffer.inodeIndex, buffer.type);
+    uint32_t inoIndex;
+    char typ;
+    while (numEntries < MAX_INODE && fread(&inoIndex, sizeof(uint32_t), 1, inoFile) == 1){
+        if (fread(&typ, sizeof(char), 1, inoFile) == 1){
+            if (typ != 'd' && typ != 'f'){
+            printf("inode file type is invalid: %u %c\n", inoIndex, typ);
             continue;
         }
-        if (buffer.inodeIndex >= MAX_INODE){
-            printf("inode number is invalid: %u\n", buffer.inodeIndex);
+        if (inoIndex >= MAX_INODE){
+            printf("inode number is invalid: %u\n", inoIndex);
             continue;
         }
         if (numEntries < MAX_INODE){
-            entries[numEntries] = buffer;
+            entries[numEntries].inodeIndex = inoIndex;
+            entries[numEntries].type = typ;
+            printf("entry type: %c index: %d numEntries: %d\n", entries[numEntries].type, entries[numEntries].inodeIndex, numEntries);
             numEntries++;
         } else {
             printf("too many entries in inode file: %d", numEntries);
             exit(1);
+            }
+        printf("Valid inode entry: %u, Type: %c\n", inoIndex, typ);
+
         }
-        printf("Valid inode entry: %u, Type: %c\n", buffer.inodeIndex, buffer.type);
     }
     fclose(inoFile);
     printf("inodes list file was closed\n");
@@ -90,7 +100,7 @@ int main(int argc, char *argv[]) {
     //run loop forever since cmdExit will exit
     while (1){
         char cmdInput[256];
-        printf("Enter a command: ");
+        printf("\nEnter a command: ");
         if (fgets(cmdInput, sizeof(cmdInput), stdin) == NULL){
             printf("End of input detected. Exiting");
             cmdExit(entries, numEntries);
@@ -100,7 +110,7 @@ int main(int argc, char *argv[]) {
         cmdInput[strcspn(cmdInput, "\n")] = 0;
         char* token;
         char* rest = cmdInput;
-        token = strtok_r(rest, " ", &rest);
+        token = strtok(rest, " ");
 
         //check for exit command save and exit
         if(strcmp(cmdInput, "exit") == 0){
@@ -108,21 +118,153 @@ int main(int argc, char *argv[]) {
         } else if (strcmp(cmdInput, "ls") == 0){
             cmdLs(curDirInode);
         } else if (strcmp(token, "cd") == 0){
-            token = strtok_r(NULL, " ", &rest);
+            token = strtok(NULL, " ");
             if (token != NULL){
+                //pass address of curdirinode so we can change
                 cmdCd(token, &curDirInode, entries, numEntries);
             } else{
-                printf("Invalid directory given: %s \n", token);
+                printf("Invalid directory name given in main: %s \n\n", token);
+                continue;
+            }
+        } else if (strcmp(token, "touch") == 0){
+            token = strtok(NULL, " ");
+            if (token != NULL){
+                //pass address of curdirinode so we can change
+                cmdTouch(token, &curDirInode, entries, &numEntries);
+            } else{
+                printf("File name not given %s \n\n", token);
+                continue;
+            }
+        } else if (strcmp(token, "mkdir") == 0){
+            token = strtok(NULL, " ");
+            if (token != NULL){
+                //pass address of curdirinode so we can change
+                cmdMkdir(token, &curDirInode, entries, &numEntries);
+            } else{
+                printf("File name not given %s \n\n", token);
                 continue;
             }
         } else{
-            printf("Invalid command entered\n");
+            printf("Invalid command entered in main\n\n");
             continue;
         }
     }
     return 0;
 }
+void cmdMkdir(const char* dirName, uint32_t* curDirInode, inodeMeta* entries, int* numEntries){
+    //similar logic as cmdTouch but with directory vs file and . and ..
+    if (entryHuh(dirName, *curDirInode)){
+        printf("entry with name already exits, nothing will happen\n");
+        return;
+    }
+
+    uint32_t newDirIno;
+    if (findNextIno(entries, *numEntries, &newDirIno) == 1){
+        printf("no inodes are avaliable in cmdMkdir\n");
+        return;
+    }
+
+    if(updateDir(*curDirInode, newDirIno, dirName) == 1){
+        printf("error when updating directory in cmdMkdir\n");
+        return;
+    }
+
+    char dirFileName[MAX_INODE_STRING];
+    sprintf(dirFileName, "%u", newDirIno);
+    FILE* file = fopen(dirFileName, "wb");
+    if (file == NULL){
+        printf("couldn't open inodes file in cmdMkdir\n");
+        return;
+    }
+
+    //create default . and ..
+    dirContent dEntries[2];
+    memset(dEntries, 0, sizeof(dEntries));
+    //first default of .
+    dEntries[0].inodeIndex = newDirIno;
+    strncpy(dEntries[0].name, ".", MAX_NAME);
+    //second default of ..
+    //make sure to set to parent
+    dEntries[1].inodeIndex = *curDirInode;
+    strncpy(dEntries[1].name, "..", MAX_NAME);
+
+    if (fwrite(dEntries, sizeof(dEntries), 1, file) != 1){
+        printf("error writing fwrite in cmdMkdir\n");
+        fclose(file);
+        return;
+    }
+    fclose(file);
+    entries[*numEntries].inodeIndex = newDirIno;
+    entries[*numEntries].type = 'd';
+    (*numEntries)++;
+
+    printf("Directory '%s' created with inode %u.\n", dirName, newDirIno);
+}
+
+void cmdTouch(const char* fileName, uint32_t* curDirInode, inodeMeta* entries, int *numEntries){
+    if (entryHuh(fileName, *curDirInode)){
+        printf("entry with name already exits, nothing will happen\n");
+        return;
+    }
+
+    uint32_t newIno;
+    if (findNextIno(entries, *numEntries, &newIno) == 1){
+        printf("no inodes are avaliable in cmdTouch\n");
+        return;
+    }
+
+    if(updateDir(*curDirInode, newIno, fileName) == 1){
+        printf("error when updating direcotry in cmdTouch\n");
+        return;
+    }
+
+    char inoFileName[MAX_INODE_STRING];
+    sprintf(inoFileName, "%u", newIno);
+    FILE* file = fopen(inoFileName, "wb");
+    if (file == NULL){
+        printf("couldn't open inodes file in cmdTouch\n");
+        return;
+    }
+    
+    //must ust fprintf to write to file
+    fprintf(file, "%s", fileName);
+    fclose(file);
+
+    entries[*numEntries].inodeIndex = newIno;
+    entries[*numEntries].type = 'f';
+    (*numEntries)++;
+
+    printf("File '%s' created with inode %u.\n", fileName, newIno);
+
+}
+
 void cmdCd(const char* dirName, uint32_t* curDirInode, const inodeMeta* entries, int numEntries){
+    //check for . or .. default
+    if (strcmp(dirName, ".") == 0){
+        printf("No directory change since cd .\n");
+        return;
+
+    //check if cd ..
+    } else if (strcmp(dirName, "..") == 0){
+        char pcurFileName[MAX_INODE_STRING];
+        sprintf(pcurFileName, "%u", *curDirInode);
+        FILE *pdirFile = fopen(pcurFileName, "rb");
+        if (pdirFile == NULL){
+            printf("couldn't open current directory file in cmdCd %s\n", pcurFileName);
+            return;
+        }
+        dirContent pdirEntry;
+        while (fread(&pdirEntry, sizeof(pdirEntry), 1, pdirFile)){
+            if (strcmp(pdirEntry.name, "..") == 0){
+                *curDirInode = pdirEntry.inodeIndex;
+                printf("changed to parent directory\n");
+                fclose(pdirFile);
+                return;
+            }
+        }
+    }
+
+    //other regular cd types 
     //max size is inode(1023) uint_32 to string
     char curFileName[MAX_INODE_STRING];
     //convert inode num to string
@@ -130,33 +272,43 @@ void cmdCd(const char* dirName, uint32_t* curDirInode, const inodeMeta* entries,
     //read binary given the string of file name
     FILE *dirFile = fopen(curFileName, "rb");
     if (dirFile == NULL){
-        printf("couldn't open current directory file %s\n", curFileName);
-        exit(1);
+        printf("couldn't open current directory file in cmdCd %s\n", curFileName);
+        return;
     }
-
     //found? checker
     int foundHuh = 0;
     dirContent dirEntry;
     while (fread(&dirEntry, sizeof(dirContent), 1, dirFile) == 1){
         //if the name of the directory matches
         if(strcmp(dirEntry.name, dirName) == 0){
+            printf("Matching dirEntry and dirName\n");
             //check if the type is a directory given the name
-            for(int i = 0; i < numEntries; i++){
-                if (entries[i].inodeIndex == dirEntry.inodeIndex && entries[i].type == 'd'){
-                    foundHuh = 1;
-                    //change current directory to the matched one 
-                    *curDirInode = dirEntry.inodeIndex;
-                    break;
+
+            for(int i = 0; i <= numEntries; i++){
+                //printf("%c\n",entries[i].type);
+                if (entries[i].inodeIndex == dirEntry.inodeIndex){
+                    if (entries[i].type == 'd'){
+                        printf("Matching entry inodeIno and dirEntry inodeIno and entries type id d\n");
+                        foundHuh = 1;
+                        //change current directory to the matched one 
+                        *curDirInode = dirEntry.inodeIndex;
+                        break;
+                    } else if (entries[i].type == 'f'){
+                        printf("Provided name is not d type, inode: %u type%c\n", entries[i].inodeIndex, entries[i].type);
+                    } else{
+                        printf("Provided name is unknown type: %u %c\n", entries[i].inodeIndex, entries[i].type);
+                    }
                 }
             }
+            break;
         }
     }
     fclose(dirFile);
 
     if (foundHuh == 0){
-        printf("Invalid directory given: %s\n", dirName);
+        printf("Invalid directory given in cmdCd: %s\n", dirName);
     } else {
-        printf("Directory changed: %s\n", dirName);
+        printf("Directory changed to: %s Inode Number: %u\n", dirName, *curDirInode);
     }
 }
 
@@ -184,17 +336,105 @@ void cmdExit(const inodeMeta* entries, int numEntries){
     //open inodes list but with wb to write to it 
     FILE *inoFile = fopen("inodes_list", "wb");
     if (inoFile == NULL){
-        printf("couldn't open inodes file");
+        printf("couldn't open inodes file\n");
         exit(1);
     }
 
-    if(fwrite(entries, sizeof(inodeMeta), numEntries, inoFile) != numEntries){
-        printf("couldn't write to inodes file");
-        exit(1);
+    for (int i = 0; i < numEntries; i++){
+        if (fwrite(&entries[i].inodeIndex, sizeof(uint32_t), 1, inoFile) != 1){
+            printf("couldn't write inode index to inodes file\n");
+            fclose(inoFile);
+            exit(1);
+        }
+
+        if (fwrite(&entries[i].type, sizeof(char), 1, inoFile) != 1){
+            printf("couldn't write inode index to inodes file\n");
+            fclose(inoFile);
+            exit(1);
+        }
     }
+
     fclose(inoFile);
-    printf("Successfully written inodes_list and exiting now");
+    printf("Successfully written inodes_list and exiting now\n");
     exit(0);
+}
+
+int updateDir(uint32_t curDirInode, uint32_t newIno, const char* dirName){
+    char dirFileName[MAX_INODE_STRING]; // size 5
+    sprintf(dirFileName, "%u", curDirInode);
+
+    FILE* dirFile = fopen(dirFileName, "ab");
+    if (dirFile == NULL){
+        printf("couldn't open file in updateDir\n");
+        return 1;
+    }
+    dirContent newEntry;
+    memset(&newEntry, 0, sizeof(newEntry));
+    newEntry.inodeIndex = newIno;
+
+    //make sure not longer than 32
+    size_t lenCheck;
+    if (strlen(dirName) < MAX_NAME){
+        lenCheck = strlen(dirName);
+        strncpy(newEntry.name, dirName, lenCheck);
+        newEntry.name[MAX_NAME - 1] = '\0';
+    } else {
+        lenCheck = MAX_NAME;
+        //printf("%zu", lenCheck);
+        strncpy(newEntry.name, dirName, lenCheck);
+    }
+
+    if (fwrite(&newEntry, sizeof(newEntry), 1, dirFile) != 1){
+        printf("error writing fwrite in updateDir\n");
+        fclose(dirFile);
+        return 1;
+    }
+    fclose(dirFile);
+    return 0;
+}
+
+int findNextIno(const inodeMeta* entries, int numEntries, uint32_t* nextIno){
+    char used[MAX_INODE] = {0};
+
+    for (int i = 0; i < numEntries; i++){
+        if (entries[i].inodeIndex < MAX_INODE){
+            used[entries[i].inodeIndex] = 1;
+        }
+    }
+
+    for (uint32_t j = 0; j < MAX_INODE; j++){
+        if (used[j] == 0){
+            *nextIno = j;
+            //if there is avaliable node, good
+            return 0;
+        }
+    }
+
+    //if all the inodes were used
+    return 1;
+}
+
+int entryHuh(const char* dirName, uint32_t dirIno){
+    char dirFileName[MAX_INODE_STRING];
+    sprintf(dirFileName, "%u", dirIno);
+
+    FILE *file = fopen(dirFileName, "rb");
+    if (file == NULL){
+        printf("couldn't open file in entryHuh");
+        return 1;
+    }
+
+    dirContent entry;
+    //check if there exists a matching entry
+    while (fread(&entry, sizeof(dirContent), 1, file) == 1) {
+        entry.name[MAX_NAME - 1] = '\0';
+        if(strcmp(entry.name, dirName) == 0){
+            fclose(file);
+            return 1;
+        }
+    }
+    fclose(file);
+    return 0;
 }
 
 int inoZero(const inodeMeta* entries, int numEntries){
